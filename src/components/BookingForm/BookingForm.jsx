df30f60ffcca33cd8848916a65b22ce4e594e65a
YozefKubirya <106235869+YozefKubirya@users.noreply.github.com>
2025-04-06 16:51:23 +0300

    add some styles
    add some styles

--- src/components/BookingForm/BookingForm.jsx
+++ src/components/BookingForm/BookingForm.jsx
@@ -1,5 +1,6 @@
 import css from './BookingForm.module.css'
-import { Formik,Form,Field } from 'formik'
+import { Formik,Form,Field,ErrorMessage } from 'formik'
+
 import DatePicker from 'react-datepicker'
 import * as Yup from 'yup';
 import "react-datepicker/dist/react-datepicker.css";
@@ -12,14 +13,14 @@ export const BookingForm = () => {
       bookingDate:null,
       text:''
    }
-   const bookingFormSchema = Yup.object({
+   const bookingFormSchema = Yup.object().shape({
       name: Yup.string()
         .min(2, 'Too Short!')
         .max(50, 'Too Long!')
         .required('Required'),
       email: Yup.string().email('Invalid email').required('Required'),
       bookingDate:Yup.date().required("Booking date is required"),
-      text:Yup.string()
+      text:Yup.string(),
     });
    const handleFormSubmit = (values, actions) => {
    console.log(values);
@@ -29,15 +30,20 @@ export const BookingForm = () => {
       <>
       <div className={css.container}>
          
-         <Formik initialValues={initialValues} onSubmit={handleFormSubmit}  validationSchema={bookingFormSchema}>
+         <Formik initialValues={initialValues} 
+         validationSchema={bookingFormSchema}onSubmit={handleFormSubmit}  >
          {({values, setFieldValue})=>(
             <Form className={css.booKingForm}>
             <h3 className={css.title}>Book your campervan now</h3>
             <p className={css.text}>Stay connected! We are always ready to help you.</p>
             <Field name="name" type='text'  placeholder='Name*' className={css.input}/>
+            <ErrorMessage name="name" component="span" className={css.span}/>
             <Field type='email' name="email" placeholder='Email*' className={css.input}/>
+            <ErrorMessage name="email" component="span" className={css.span}/>
                <DatePicker selected={values.bookingDate} onChange={(date) => setFieldValue("bookingDate",date)} placeholderText='Booking Date*' className={css.input}/>
+               <ErrorMessage name="bookingDate" component="span" className={css.span}/>
             <Field as='textarea' name="text" placeholder='Comment*' className={css.input}/>
+            <ErrorMessage name="textarea" component="span" className={css.span}/>
             <button type='submit' className={css.sendButton}>Send</button>
          </Form>
          )}   
--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -6,6 +6,9 @@ import css from './Loader.module.css'
 export const Loader = () => {
    
    return (<>
+   <div className={css.loaderContainer}>
    <p className={css.loader}>Please wait , Truck Apllication is loading...</p>
+   </div>
+   
    </>)
 }
