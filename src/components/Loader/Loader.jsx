088b397f478f522e923b4f135b7f57879ab2561f
YozefKubirya <106235869+YozefKubirya@users.noreply.github.com>
2025-04-11 09:05:47 +0300

    add loader
    add loader

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,13 +1,18 @@
 
 
 import css from './Loader.module.css'
-
+import ClipLoader from "react-spinners/ClipLoader";
 
 export const Loader = () => {
    
    return (<>
    <div className={css.loaderContainer}>
-   <p className={css.loader}>Please wait , Truck Apllication is loading...</p>
+      <ClipLoader 
+        className={css.loader}
+        size={150}
+        aria-label="Loading Spinner"
+        data-testid="loader"  
+        />   
    </div>
    
    </>)
